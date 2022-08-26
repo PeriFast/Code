051b08071bfa7957eef6c5f0c8ac3e54fb88ca2c
Farzaneh-Mousavi <farzaneh.mousavi@huskers.unl.edu>
2022-08-26 17:36:04 -0500

    Update update_tractions.m

--- PeriFast_Dynamics/update_tractions.m
+++ PeriFast_Dynamics/update_tractions.m
@@ -1,15 +1,15 @@
-function [btx,bty,btz] =...
-    update_tractions(trac_x,trac_y,trac_z,chit_x,chit_y,chit_z,...
-    delta,Nx,Ny,Nz,X,Y,Z,t,dy)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This module enforces traction BCs as body forces applied uniformly on
+% delta-thickness boundary layer
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Assemble body forces for each coordinate direction
 btx = assemble_BC(chit_x,trac_x,Nx,Ny,Nz,X,Y,Z,t)/delta;
 bty = assemble_BC(chit_y,trac_y,Nx,Ny,Nz,X,Y,Z,t)/delta;
-% bty = assemble_BC(chit_y,trac_y,Nx,Ny,Nz,X,Y,Z,t)/dy;
-
 btz = assemble_BC(chit_z,trac_z,Nx,Ny,Nz,X,Y,Z,t)/delta;
 
-end
 
+% function for assembling bt_i
 function combined_BC = assemble_BC(BC_sets,BC_values,Nx,Ny,Nz,X,Y,Z,t)
 combined_BC = zeros(Ny,Nx,Nz);
 n = BC_sets.No;
