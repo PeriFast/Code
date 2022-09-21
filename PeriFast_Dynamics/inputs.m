f81fa23be9dcc3ea3863b2aaf212de1c05f13ad3
Farzaneh-Mousavi <farzaneh.mousavi@huskers.unl.edu>
2022-09-20 19:15:37 -0500

    Update inputs.m

--- PeriFast_Dynamics/inputs.m
+++ PeriFast_Dynamics/inputs.m
@@ -4,22 +4,32 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Physical inputs:
-props = [1; 2440; 3.8; 72e9; 0.25]; % material properties
+props = [2; 2440; 3.8; 72e9; 0.25]; % material properties
 % props (1): mat_type (0:BB, 1: Linearized SB, 2: correspondence SB)
-% props (2): rho(density) 
-% props (3): G0 (fracture energy release rate)
-% props (4): E (Young's modulus)
+% props (2): rho(density) , unit in kg/m^3
+% props (3): G0 (fracture energy release rate), unit in J/m^2
+% props (4): E (Young's modulus), unit in  Pa
 % props (5): nu (Poisson ratio if not bond-based)
 
-t_max = 33e-6; % maximum time
-dt = 5e-8; % time step
+t_max = 33e-6; % maximum time, unit in s
+% safty check for t_max
+if(t_max <= 0)
+    error('t_max must be a positive number');
+end
+dt = 5e-8; % time step (should satisfy the stability condition), unit in s
+%safty check for dt
+if(dt > t_max)
+    error('dt can not large than the total time');
+end
+
+run_in_gpu = 0; % run in gpu:1, do not run in gpu:0;
 
 % output parameters
 snap = 5; % number of steps between snapshots(dumping output data)
-tecplot_output = 1 ; % provide tecplot file :1,  Do not provide tecplot file:0
+tecplot_output = 0 ; % provide tecplot file :1,  Do not provide tecplot file:0
 
 % visualization parameter
-plot_output = 1 ; % plot output(Matlab plot) during analysis:1,  Do not plot:0
+visualization_during_analysis = 1 ; % Matlab plot/animate output during analysis:1,  Do not plot/animate:0
 
 %%% Body force density functions:
 Fb(1).func = @(x,y,z,t)0;
