70b46af2618cbcc159c76bc482064f9e1f1e6ccd
Farzaneh-Mousavi <farzaneh.mousavi@huskers.unl.edu>
2022-09-20 19:15:02 -0500

    Create close_Matlab_video.m

---
+++ PeriFast_Dynamics/close_Matlab_video.m
@@ -0,0 +1,9 @@
+% close  videos 
+% close damage video 
+close(damage_video);
+
+% one can easily add other outputs to be animated
+
+% close(vel1_video);
+% close(vel2_video);
+% close(energy_video)
