c7e5e15cef50f045a0943ec750dbb7ce6f76a82f
Farzaneh-Mousavi <farzaneh.mousavi@huskers.unl.edu>
2022-09-20 19:17:02 -0500

    Create open_Matlab_video.m

---
+++ PeriFast_Dynamics/open_Matlab_video.m
@@ -0,0 +1,14 @@
+% open  videos 
+
+% openn damage video
+damage_video = VideoWriter('damage.mp4','MPEG-4');
+open(damage_video);
+
+% one can easily add other outputs to be animated
+
+% vel1_video = VideoWriter('vel1.mp4','MPEG-4');
+% open(vel1_video);
+% vel2_video = VideoWriter('vel2.mp4','MPEG-4');
+% open(vel2_video);
+% energy_video = VideoWriter('energy.mp4','MPEG-4');
+% open(energy_video)
