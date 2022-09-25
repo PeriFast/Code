fc288a8cf3fddb1a4de4c7f1e0c25b10d5e259a9
Farzaneh-Mousavi <farzaneh.mousavi@huskers.unl.edu>
2022-09-25 16:24:43 -0500

    Update visualization.m

--- PeriFast_Dynamics/visualization.m
+++ PeriFast_Dynamics/visualization.m
@@ -12,66 +12,114 @@ x_plot = X(plot_nodes); %  coorniates of nodes in x-direction
 y_plot = Y(plot_nodes); %  coorniates of nodes in y-direction
 z_plot = Z(plot_nodes); %  coorniates of nodes in z-direction
 
-% output selection for plots
 u1_plot = Output(ks).u1(plot_nodes);  % displacement in x- direction 
 u2_plot = Output(ks).u2(plot_nodes);  % displacement in y- direction
 u3_plot = Output(ks).u3(plot_nodes);  % displacement in z- direction
-v1_plot = Output(ks).v1(plot_nodes);  % velocity in x- direction
-v2_plot = Output(ks).v2(plot_nodes);  % velocity in y- direction
-v3_plot = Output(ks).v3(plot_nodes);  % velocity in z- direction
-damage_plot = Output(ks).d(plot_nodes); % damage index
-energy_plot = Output(ks).W(plot_nodes);  % strain energy density
 
 % nodal coordinates in current configurartion
 xcur_plot = x_plot + u1_plot;
 ycur_plot = y_plot + u2_plot;
 zcur_plot = z_plot + u3_plot;
 
-% plot damage index as figure (1)
-figure(1)
-scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,damage_plot(:));
-colormap jet
-axis equal;
-caxis([0 1])
-title ( sprintf ('d ,t = %1.2e sec',dt*ks*5));
+% output selection for plots
+% visualization_var.output1;
+% num_of_outputs = visualization_var.No;
+num_of_outputs= size(outputs_var_for_visualization,2);
+
+for i = 1:num_of_outputs
+figure(i);
+
+outFilename = sprintf('output%d', i);
+
+out_var = outputs_var_for_visualization(1,i);
+if out_var  == 1
+    output_plot = Output(ks).u1(plot_nodes);
+    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('u1 ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 2
+    output_plot = Output(ks).u2(plot_nodes);
+    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('u2 ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 3
+    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    output_plot = Output(ks).u3(plot_nodes);
+    title ( sprintf ('u3 ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 4
+    output_plot = Output(ks).u_mag(plot_nodes);
+    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('u_mag ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 5
+    output_plot = Output(ks).v1(plot_nodes);
+    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    caxis([-1 1]);
+    title ( sprintf ('v1 ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 6
+    output_plot = Output(ks).v2(plot_nodes);
+        scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    caxis([-3.5 3.5])
+    title ( sprintf ('v2 ,t = %1.2e sec',dt*ks*snap));
+    
+end
+if out_var  == 7
+    output_plot = Output(ks).v3(plot_nodes);
+        scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('v3 ,t = %1.2e sec',dt*ks*snap));
+end
+
+if out_var  == 8
+    output_plot = Output(ks).v_mag(plot_nodes);
+        scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('v_mag ,t = %1.2e sec',dt*ks*snap));
+end
+if out_var  == 9
+    output_plot = Output(ks).W(plot_nodes);    scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+        caxis([0 1500])
+    title ( sprintf ('W ,t = %1.2e sec',dt*ks*snap));
+
+end
+if out_var  == 10
+    output_plot = Output(ks).d(plot_nodes);
+        scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    caxis([0 1]);
+    title ( sprintf ('damage ,t = %1.2e sec',dt*ks*snap));
+    
+end
+if out_var  == 11
+    output_plot = Output(ks).lambda(plot_nodes);
+        scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,output_plot(:));
+    colormap jet
+    axis equal;
+    title ( sprintf ('lambda ,t = %1.2e sec',dt*ks*snap));
+end
 colorbar;
 drawnow
-
-
-
-%%% user can uncomment the part below to plot other outputs:
-
-% plot v1 as figure (2), velocity in x direction
-% figure(2)
-% scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,v1_plot(:));
-% colormap jet
-% axis equal;
-% caxis([-1 1])
-% title ( sprintf ('v1 , t = %1.2e sec',t));
-% colorbar;
-% drawnow
-% 
-% plot v2  as figure (3), velocity in ydirection
-% figure(3)
-% scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,v2_plot(:));
-% colormap jet
-% axis equal;
-% caxis([-3.5 3.5])
-% title ( sprintf ('v2 , t = %1.2e sec',t));
-% colorbar;
-% drawnow
-% 
-% plot strain energy density  figure (4), 
-% figure(4)
-% scatter3(xcur_plot(:),ycur_plot(:),zcur_plot(:), 5,energy_plot(:));
-% colormap jet
-% axis equal;
-% caxis([0 1500])
-% title ( sprintf ('W , t = %1.2e sec',t));
-% colorbar;
-% drawnow
-
+end
 
 % create movies from the frams (snapshots)
-create_Matlab_video(damage_video);
+create_Matlab_video;
 
